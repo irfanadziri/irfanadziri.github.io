65fdb0fb8e999453ea5b4149969276af13a453ea
irfanadziri <irfanadziri@users.noreply.github.com>
2024-04-07 08:03:35 +0000

    deploy: irfanadziri/portfolio-v2@647b9c47db01933c8af51f037389b6b1bd6c0970

--- _next/static/chunks/app/projects/portfolio-v2/page-d93f5b6f2499b694.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[461,679,615,82,802,217,942,944,460,351],{8677:function(n,e,u){Promise.resolve().then(u.t.bind(u,1749,23))}},function(n){n.O(0,[749,971,69,744],function(){return n(n.s=8677)}),_N_E=n.O()}]);
